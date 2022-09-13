f21291656c0d197a1283a87947be6ef8c08d432c
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-12 19:03:26 -0700

    Task 4.

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print all lowecase alphabets except q and e
+ * Return: always 0
+ */
+
+int main(void)
+{
+	char = 'a';
+
+	for (char = 'a'; char <= 'z'; char++)
+	{
+		putchar(char);
+	}
+	putchar('\n');
+	Reurn(0);
+}
