94a5846f308fce8f52348111ff842a04fa175f3f
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-12 20:02:12 -0700

    Task 6.

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: A program to print all numbers
+ * Return: always 0
+ */
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 9)
+	{
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
