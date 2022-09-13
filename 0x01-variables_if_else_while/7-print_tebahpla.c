5c259ab1ac45405248756d896d06ac952e0390ef
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-12 20:43:59 -0700

    Task 7.

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -2,16 +2,16 @@
 /**
  * main - Entry point
  *
- * Description: A program to print all numbers
+ * Description: A program to print lowercase alphabets in reverse
  * Return: always 0
  */
 int main(void)
 {
-	int num = '0';
+	char c = 'z';
 
-	for (num = '0'; num <= '9'; num++)
+	for (c = 'z'; c <= 'a'; c--)
 	{
-		putchar(num);
+		putchar(c);
 	}
 	putchar('\n');
 	return (0);
