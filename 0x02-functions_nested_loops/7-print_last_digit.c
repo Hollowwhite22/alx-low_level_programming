3f2f4f496ca980dcf1b6f19ef3a09cb42e7f5b23
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-15 11:06:31 -0700

    Task 7.

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - Function prototype
+ * @n: integer whose last digit we want to find
+ *
+ * Return: int
+ */
+int print_last_digit(int n)
+{
+	if (n < 0)
+	{
+		last_digit = -1 * (n % 10);
+		_putchar(last_digit + 48);
+		return (last_digit);
+	}
+	else
+	{
+		last_digit = n % 10;
+		_putchar(last_digit + 48);
+		return (last_digit);
+	}
+}
