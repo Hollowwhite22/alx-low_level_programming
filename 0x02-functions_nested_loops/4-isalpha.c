cef73cc4f126e1e70dc940d3750420933ec53dac
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-15 06:08:15 -0700

    Task 4.

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _isalpha - Function prototype
+ * @c: Integer representing the ascii values
+ * Return: 1 if is a letter, lowercase or uppercase and 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 123) || (c >= 65 && c >= 91))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
