acb6a3a40747d462f2d4f95e4528df2fd24d6057
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-15 06:49:25 -0700

    Task 6.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - Function prototype
+ * @n: integer whose absolute value to find
+ * Return: int
+ */
+int _abs(int n)
+{
+	if (n < o)
+	{
+		return (n * -1);
+	}
+	else
+	{
+		return (n);
+	}
+}
