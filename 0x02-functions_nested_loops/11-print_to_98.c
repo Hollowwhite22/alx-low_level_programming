8c008663abad4d9186ea909191630466a4930716
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-16 00:54:39 -0700

    Task 11.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - Function prototype
+ * @n: integer
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+
+			if (n != 98)
+			{
+				printf(", ");								}
+			n--;
+		}
+	}
+	else
+	{
+		while (n <= 98)
+		{											printf("%d", n);
+
+			if (n != 98)
+				printf(", ");
+			n++;
+		}
+	}
+	printf("\n");
+}
