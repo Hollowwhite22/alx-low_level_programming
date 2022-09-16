934770758e8fed9d30053ed13ba64f53ac59c5c3
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-16 02:35:34 -0700

    Task 13.

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,19 +5,19 @@
  */
 int main(void)
 {
-	int sum3 = 0, sum5 = 0;
+	int sum = 0;
 
 	int i;
 
 	for (i = 0; i < 1024; i++)
 	{
 		if (i % 3 == 0)
-			sum3 += i;
+			sum += i;
 
 		if (i % 5 == 0)
-			sum5 += i;
+			sum += i;
 	}
-	printf("%d\n", sum3 + sum5);
+	printf("%d\n", sum);
 
 	return (0);
 }
