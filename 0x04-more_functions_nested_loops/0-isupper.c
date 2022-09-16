d367611bc39b405875bb9d150a119f1181214354
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-16 11:47:50 -0700

    Task 0.

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
  * _isupper - Function that checks for uppercase character
  * @c: int type number
- * Return: 1 if c is uppercase, 0 if otherwise
+ * Return: 1 if c is uppercase, 0 otherwise
  */
 
 int _isupper(int c)
