f1dfdc6f803b63245855371c46d3fc14acd6df11
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-15 23:59:26 -0700

    Task 10.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - Function prototype
+ * @a: first integer
+ * @b: second integer
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	int sum = (int a + int b);
+
+	return (sum);
+}
