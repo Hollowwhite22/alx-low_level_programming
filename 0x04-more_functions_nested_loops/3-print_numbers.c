a42a7c032dbee538db7200ad15af5eae6ae429ae
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-16 22:29:44 -0700

    Task 3.

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * print_numbers - A function that print numbers from 0 to 9
  *
- * @i: integer representing ascii value
+ *
  * Return: void
  */
 
 void print_numbers(void)
 {
-	int i = 48;
+	int c = 48;
 
-	while (i < 58)
+	while (c < 58)
 	{
-		_putchar(i);
-		i++;
+		_putchar(c);
+		c++;
 	}
 	_putchar('\n');
 }
