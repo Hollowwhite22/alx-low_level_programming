babb5794ae3cd2a5cb1d1669ab3e7595615c12a1
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-18 07:48:34 -0700

    Task 9.

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+int main()
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0)
+			putchar("Fizz ");
+		else if (n % 5 == 0)
+			putchar("Buzz ");
+		else if ((n % 3 == 0) && (n % 5 == 0))
+			putchar("FizzBuzz ");
+		else
+			printf("%d ", n);
+	}
+}
