85ab5ea88b7b47a9554bd5e3b409bfbaadfa6d64
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-18 09:23:03 -0700

    Task 10.

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_triangle - A function that prints a triangle, followed by a new line
+ * @size: integer of the triangle size
+ * Return: void
+ */
+
+void print_triangle(int size);
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= i; j++)
+		{
+			_putchar(" ");
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	return (0);	
+}
