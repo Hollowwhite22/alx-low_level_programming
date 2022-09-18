d10f6cf1d7caa3d036d03071b83c6b3df809911c
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-18 09:35:31 -0700

    Task 7.

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -18,14 +18,12 @@ void print_diagonal(int n)
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 1; j <= i; j++)
 			{
-				if (j == i)
-					_putchar(92);
-				else
-					_putchar(' ');
+				_putchar(92);
+				_putchar(' ');
 			}
 			_putchar('\n');
 		}
