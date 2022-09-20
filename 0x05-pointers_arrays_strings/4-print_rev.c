762e4999423234cb6b1c068d29fca0c72c74e365
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-20 14:58:11 -0700

    Task 4.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - A function that prints a string in reverse
+ *
+ * @s - pointer variable
+ * Return: void
+ */
+
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = 0; *(s + len) != '\0'; len---)
+		_putchar(s + len);
+	_putchar('\n');
+}
