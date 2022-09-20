4c8fc4cfb1907670e33785b5acf1199a97c8f5aa
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-20 01:24:25 -0700

    Task 2.

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - A function that returns the length of a string
+ *
+ * @s: pointer variable
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	while (*s != '\0')
+		s++
+
+	return (s);
+}
