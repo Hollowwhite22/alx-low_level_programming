9817910cf851ea8d0ee4343e4a726538e1ce984c
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-20 15:31:39 -0700

    Task 4.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,9 +9,21 @@
 
 void print_rev(char *s)
 {
-	int len;
-
-	for (len = 0; *(s + len) != '\0'; len--)
-		_putchar(*(s + len));
+	int len = 0, i, length;
+	
+	char c;
+	
+	while (*(s + len) != '\0')
+	{
+		len++;
+	}
+	length = len;
+	len--;
+	for (i = 0; i < length / 2; i++)
+	{
+		c = *(s + i);
+		*(s + i) = *(s + len);								*(s + len) = c;
+		len--;
+	}
 	_putchar('\n');
 }
