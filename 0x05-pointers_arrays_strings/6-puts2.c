f213f528eb6c79b9d6f23d04727d2949e3d4d46e
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-20 17:05:50 -0700

    Task 6.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - A function that prints every other character of a string
+ * starting with the firstcharacter
+ * @str: pointer variable
+ * Return: void
+ */
+
+void puts2(char *str);
+{
+	int len;
+
+	for (len = 0; *(str + len) != '\0'; len++)
+	{
+		if (len % 2 == 0)
+			_putchar(*(str + len));
+	}
+	_putchar('\n');
+}
