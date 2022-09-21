057e1ab70c8eeff0c10757d97f06d1d51bba7644
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-20 17:28:39 -0700

    Task 7.

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,5 +8,20 @@
 
 void puts_half(char *str)
 {
+	int len = 0;
 
+	while (*(str + len) != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		len /= 2;
+	else
+		len = len / 2 + 1;
+
+	while (*(str + len) != '\0')
+	{
+		_putchar(*(str + len));
+		len++;
+	}
+	_putchar('\n');
 }
