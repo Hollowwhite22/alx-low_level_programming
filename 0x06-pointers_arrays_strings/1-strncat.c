e943f8c34269c6943bba00d1be4ece028b60d233
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-23 15:26:03 -0700

    Task 1.

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strncat - A function that concatenates two strings
+ * 
+ * @dest: A pointer to a character array
+ * @src: A pointer to a character array
+ * @n: number of characters to be added
+ *
+ * Return: character pointer
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *p, i = 0, j = 0;
+
+	while (*(dest + i) != '\0')
+		i++;
+
+	while (!(*(src + j) = '\0' || j == n))
+	{
+		*(dest + i) = *(src + j);
+		i++;
+		j++;
+	}
+	*(dest + i) = '\0';
+
+	p = dest;
+
+	return (p);
+}
