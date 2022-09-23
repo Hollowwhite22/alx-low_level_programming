fb885eb07f9b1d3fb75c0424dc1786ad283b64c3
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-23 14:25:26 -0700

    Task 9.

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,16 +11,28 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; n <= 100; n++)
+	for (n = 1; n < 101; n++)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
-			printf("FizzBuzz ");
+		{
+			printf("FizzBuzz");
+			printf(" ");
+		}
 		else if (n % 3 == 0)
-			printf("Fizz ");
+		{
+			printf("Fizz");
+			printf(" ");
+		}
 		else if (n % 5 == 0)
-			printf("Buzz ");
+		{
+			printf("Buzz");
+			printf(" ");
+		}
 		else
-			printf("%d ", n);
+		{
+			printf("%d", n);
+			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
