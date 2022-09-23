9a2c9ee97f7dc24f7a9959959e68f671df45978d
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-23 07:34:50 -0700

    Task 0.

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,9 +9,10 @@
 
 char *_strcat(char *dest, char *src)
 {
-	char *p, i, j;
+	char *p, i = 0, j;
 
-	for (i = 0; *(dest + i) != '\0'; i++)
+	while (*(dest + i) != '\0')
+		i++;
 
 	for (j = 0; *(src + j) != '\0'; i++, j++)
 	{
