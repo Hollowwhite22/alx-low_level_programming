0b6684c01477a393ef50e2aadfcac6b935db8cbe
Joshua Kpamiose Ngodwin <joshuagodwin678@gmail.com>
2022-09-27 05:17:13 -0700

    Task 0.

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memset - fills memory with a constant byte
+ *
+ * @s: pointer to string
+ * @b: constant byte
+ * @n: byte to change
+ * Return: pointer to the modified string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned char p = s;
+
+	while (n--)
+	{
+		*p++ = b;
+	}
+	return s;
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Extra practise on arrays and pointers - 0x07-pointers_arrays_strings
